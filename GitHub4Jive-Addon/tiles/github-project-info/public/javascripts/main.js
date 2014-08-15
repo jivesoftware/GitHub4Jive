74a91a595febb11b71f72c575714cfdf1ffb37d2
Glen Nicol <glen.nicol@live.com>
2014-08-15 13:48:05 -0700

    Ported Auth code over to project info tile

--- GitHub4Jive-Addon/tiles/github-project-info/public/javascripts/main.js
+++ GitHub4Jive-Addon/tiles/github-project-info/public/javascripts/main.js
@@ -1,167 +1,268 @@
-var app = {
-  currentView : gadgets.views.getCurrentView().getName(),
+var place, previousRepo;
+var jiveDone = false;
+var githubDone = false;
 
-  initGadget : function() {
-    console.log('initGadget called...');
-        
-    jive.tile.onOpen(function(config, options ) {
-        gadgets.window.adjustHeight();
 
-        if ( typeof config === "string" ) {
-            config = JSON.parse(config);
-        }
 
-        var json = config || {
-            "startSequence": "1"
-        };
 
-        // prepopulate the sequence input dialog
-        $("#start_sequence").val( json["startSequence"]);
-
-        $("#btn_submit").click( function() {
-            config["startSequence"] = $("#start_sequence").val();
-            jive.tile.close(config, {} );
-            gadgets.window.adjustHeight(300);
-        });
-    });
-    
-  },
-  
-  initjQuery : function(host) {
-    console.log('initjQuery called: ',host);
-    
-    var options = {
-        serviceHost : host,
-        grantDOMElementID : '#oauth',
-        jiveAuthorizeUrlErrorCallback : app.jiveAuthorizeUrlErrorCallback,
-        oauth2SuccessCallback : app.oauth2SuccessCallback,
-        preOauth2DanceCallback : app.preOauth2DanceCallback,
-        onLoadCallback : app.onLoadCallback,
-        authorizeUrl : host + '/github/oauth/authorize'
-    };
-    
-    $("#btn_done").click( function() {
-        console.log(onLoadContext);
-    });
-    
-    OAuth2ServerFlow( options ).launch();
-    
-  },
-  
-  tokenErrorCallback : function() {
-    alert('tokenErrorCallback error');
-  },
-  
-  jiveAuthorizeUrlErrorCallback : function() {
-    alert('jiveAuthorizeUrlErrorCallback error');
-  },
-  
-  preOauth2DanceCallback : function() {
-    $("#j-card-authentication").show();
-    $("#j-card-configuration").hide();
-    gadgets.window.adjustHeight(350);
-  },
-
-  onLoadCallback : function( config, identifiers ) {
-    onLoadContext = {
-        config: config,
-        identifiers : identifiers
-    };
-  },
-
-  oauth2SuccessCallback : function(accessToken) {
-    console.log('accessToken:',accessToken);
-
-    // do configuration
+function AllAuthorized() {
     $("#j-card-authentication").hide();
     $("#j-card-configuration").show();
     gadgets.window.adjustHeight(350);  // do this here in case the pre-auth callback above wasn't called
 
-    //debugger;
-    var identifiers = jive.tile.getIdentifiers();
-    var viewerID = identifiers['viewer'];   // user ID
-    
-/* SAMPLE RESPONSE
-    [
-      {
-        "id": 1296269,
-        "owner": {
-          "login": "octocat",
-          "id": 1,
-          "avatar_url": "https://github.com/images/error/octocat_happy.gif",
-          "gravatar_id": "somehexcode",
-          "url": "https://api.github.com/users/octocat",
-          "html_url": "https://github.com/octocat",
-          "followers_url": "https://api.github.com/users/octocat/followers",
-          "following_url": "https://api.github.com/users/octocat/following{/other_user}",
-          "gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
-          "starred_url": "https://api.github.com/users/octocat/starred{/owner}{/repo}",
-          "subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
-          "organizations_url": "https://api.github.com/users/octocat/orgs",
-          "repos_url": "https://api.github.com/users/octocat/repos",
-          "events_url": "https://api.github.com/users/octocat/events{/privacy}",
-          "received_events_url": "https://api.github.com/users/octocat/received_events",
-          "type": "User",
-          "site_admin": false
-        },
-        "name": "Hello-World",
-        "full_name": "octocat/Hello-World",
-        "description": "This your first repo!",
-        "private": false,
-        "fork": false,
-        "url": "https://api.github.com/repos/octocat/Hello-World",
-        "html_url": "https://github.com/octocat/Hello-World"
-      }
-    ]
-*/  
+    // set up a query to get this user's list of repositories
     osapi.http.get({
-      href: 'https://api.github.com/repositories',
-      format: 'json',
-      headers: {  'Authorization' : 'token ' + accessToken  },
-      noCache: true,
-      authz: 'signed'
-    }).execute(function( response ) {
-      console.log('Received GitHub Response:',response.status);
-
-      var config = onLoadContext['config'];
-
-      if ( response.status >= 400 && response.status <= 599 ) {
-        alert("ERROR!" + JSON.stringify(response.content));
-      } // end if
-      
-      var data = response.content;
-      console.log(data);
-
-      // could use a forEach or something here ...
-      $.each(data,function(index,repo) {
-        $("#repoList").append(
-          "<option value=\"" + repo.id +  "\"" +
-          ((repo.name == config['repo']) ? " " : " selected ") +
-          "data-url=\""+repo.url+"\"" + 
-          "data-description=\""+repo.description+"\"" + 
-          ">" + repo.full_name +"</option>"
-        );
-      }); // end for
-
-      $("#btn_submit").click( function() {
-
-       var toReturn = {
-          "repo" : $("#repoList option:selected").text(),
-          "id"   : $("#repoList option:selected").val(),
-          "url"  : $("#repoList option:selected").attr('data-url'),
-          "description" : $("#repoList option:selected").attr('data-description'),
-          "isGitHub" : true
-        };
-        
-       console.log("toReturn", toReturn);
-        
-       jive.tile.close(toReturn);
-      });
+        'href': host + '/github/user/repos?' +
+            "&ts=" + new Date().getTime() +
+            "&place=" + encodeURIComponent(place.resources.self.ref),
+        //"&query=" + query,
+        'format': 'json',
+        'authz': 'signed'
+    }).execute(function (response) {
+        if (response.status >= 400 && response.status <= 599) {
+            alert("ERROR!" + JSON.stringify(response.content));
+        }
+        var data = response.content;
+        for (var i = 0; i < data.length; i++) {
+            var opt;
+            var name = data[i].fullName;
+            if (name === previousRepo) {
+                opt = "<option value=" + data[i].name + " selected>" + data[i].fullName + "</option>";
+            } else {
+                opt = "<option value=" + data[i].name + ">" + data[i].fullName + "</option>";
+            }
+            $("#projectList").append(opt);
+        }
     });
+}
 
-    gadgets.window.adjustHeight();
-  }
+function BothAreDone() {
+    return jiveDone && githubDone;
+}
 
+function ProceedWhenReady() {
+    if (BothAreDone()) {
+        AllAuthorized();
+    }
+}
+
+function setupOAuthFor(system, successCallBack) {
+    var ticketErrorCallback = function () {
+        console.log('ticketErrorCallback error');
+    };
+
+    var jiveAuthorizeUrlErrorCallback = function () {
+        console.log('jiveAuthorizeUrlErrorCallback error');
+    };
+
+    var preOauth2DanceCallback = function () {
+        console.log("preOauth2DanceCallback");
+    };
+
+    var onLoadCallback = function (config, identifiers) {
+        console.log("onLoadCallback");
+    };
+
+    var authorizeUrl = host + '/' + system + '/oauth/authorize';
+    var viewerID = new Date().getTime();
+
+    OAuth2ServerFlow({
+        serviceHost: host,
+        grantDOMElementID: '#github4jive-' + system + '-authorize',
+        ticketErrorCallback: ticketErrorCallback,
+        jiveAuthorizeUrlErrorCallback: jiveAuthorizeUrlErrorCallback,
+        oauth2SuccessCallback: successCallBack,
+        preOauth2DanceCallback: preOauth2DanceCallback,
+        onLoadCallback: onLoadCallback,
+        authorizeUrl: authorizeUrl,
+        jiveOAuth2Dance: system === "jive",
+        context: {"place": place.resources.self.ref}
+    }).launch({'viewerID': viewerID});
+}
+
+var app = {
+
+    currentView: gadgets.views.getCurrentView().getName(),
+    currentViewerID: -1,
+    initGadget: function () {
+        console.log('initGadget ...');
+
+        gadgets.actions.updateAction({
+            id: "com.jivesoftware.addon.github4jive.group.config",
+            callback: app.handleContext
+        });
+
+        gadgets.actions.updateAction({
+            id: "com.jivesoftware.addon.github4jive.project.config",
+            callback: app.handleContext
+        });
+
+        gadgets.actions.updateAction({
+            id: "com.jivesoftware.addon.github4jive.space.config",
+            callback: app.handleContext
+        });
+
+        jive.tile.onOpen(function(config, options ) {
+            gadgets.window.adjustHeight();
+
+            if ( typeof config === "string" ) {
+                config = JSON.parse(config);
+            }
+
+            var json = config || {
+                "startSequence": "1"
+            };
+
+            // prepopulate the sequence input dialog
+            $("#start_sequence").val( json["startSequence"]);
+
+            $("#btn_submit").click( function() {
+                config["startSequence"] = $("#start_sequence").val();
+                jive.tile.close(config, {} );
+                gadgets.window.adjustHeight(300);
+            });
+        });
+    },
+
+    initjQuery: function () {
+        console.log('initjQuery ...');
+        gadgets.window.adjustHeight(250);
+    },
+
+    handleContext: function (context) {
+        console.log('handleContext ...');
+        if (context) {
+
+            osapi.jive.corev3.resolveContext(context, function (result) {
+                place = result.content;
+
+                setupOAuthFor("github", function (ticketID) {
+                    if (ticketID) {
+                        githubDone = true;
+                        $('#github4jive-github-authorize').slideUp('fast');
+                        $('#github4jive-github-authorize-success').slideDown('fast', ProceedWhenReady);
+                        $('#github4jive-github-token').val(ticketID);
+
+                    }
+                });
+
+                setupOAuthFor("jive", function (ticketID) {
+                    if (ticketID) {
+                        jiveDone = true;
+                        $('#github4jive-jive-authorize').slideUp('fast');
+                        $('#github4jive-jive-authorize-success').slideDown('fast', ProceedWhenReady);
+                        $('#github4jive-jive-token').val(ticketID);
+                    }
+                });
+
+                result.content.getExtProps().execute(function (props) {
+
+                    if ("true" === props.content.github4jiveEnabled) {//&& props.content.github4jiveGitHubAccessToken && props.content.github4jiveJiveAccessToken) {
+                        console.log('initializing UI for already configured place');
+                        previousRepo = props.content.github4jiveRepoOwner + "/" + props.content.github4jiveRepo;
+                    } else {
+                        console.log('initializing UI for UNconfigured place');
+                    }
+
+                    //double check server side configuration with ext props
+                    debugger;
+                    osapi.http.get({
+                        'href': host + '/jive/place/isConfigured?' +
+                            "&ts=" + new Date().getTime() +
+                            "&place=" + encodeURIComponent(place.resources.self.ref),
+                        //"&query=" + query,
+                        'format': 'json',
+                        'authz': 'signed'
+                    }).execute(function (response) {
+                            var config = response.content;
+                            debugger;
+                            githubDone = config.github;
+                            jiveDone = config.jive;
+
+                            if (BothAreDone()) {
+                                AllAuthorized();
+                            } else {
+                                if (config.github) {
+                                    $('#github4jive-github-authorize-success').slideDown('fast', function () {
+                                    });
+                                }
+                                else {
+                                    $('#github4jive-github-authorize').slideDown('fast', function () {
+                                    });
+                                }
+
+                                if (config.jive) {
+                                    $('#github4jive-jive-authorize-success').slideDown('fast', function () {
+                                    });
+                                }
+                                else {
+                                    $('#github4jive-jive-authorize').slideDown('fast', function () {
+                                    });
+                                }
+                            }
+                        }
+                    );
+
+                });
+
+            });
+
+            $("#github4jive-enable-submit").click(function () {
+                console.log('clicked');
+                osapi.jive.corev3.resolveContext(context, function (result) {
+                    console.log('resolveContext callback');
+                    if (result.content) {
+                        console.log('context has content callback');
+                        //TODO: BULLET-PROOF/UN HARD CODE THE LOGIC HERE, REVISIT ONCE THE FLOW IS BETTER BAKED - RR
+                        var fullName = $("#projectList option:selected").text();
+                        var parts = fullName.split("/");
+                        var owner = parts[0];
+                        var repoName = parts[1];
+
+                        result.content.createExtProps({
+                            "github4jiveEnabled": true,
+                            "github4jiveGitHubAccessToken": $("#github4jive-github-token").val(),
+                            "github4jiveJiveAccessToken": $("github4jive-jive-token").val(),
+                            "github4jiveRepo": repoName,
+                            "github4jiveRepoOwner": owner
+                        }).execute(function (resp) {
+                            console.log('resp: {' + JSON.stringify(resp) + '}');
+                            osapi.jive.core.container.closeApp();
+                        });
+
+                        osapi.http.post({
+                            'href': host + "/github/place/trigger?" +
+                                "&place=" + encodeURIComponent(place.resources.self.ref),
+                            'format': 'json',
+                            'authz': 'signed'
+                        }).execute(function (response) {
+                            console.log(response);
+                        });
+                    }
+                });
+            });
+        }
+    }
 };
 
 gadgets.util.registerOnLoadHandler(gadgets.util.makeClosure(app, app.initGadget));
+
+
+
+// register a listener for embedded experience context
+opensocial.data.getDataContext().registerListener('org.opensocial.ee.context', function (key) {
+    var data = opensocial.data.getDataContext().getDataSet(key);
+
+
+    var resolverTransform = data.container;
+    if(resolverTransform.type == 600){
+        resolverTransform.type = "osapi.jive.core.Project";
+    }
+    if(resolverTransform.type == 700){
+        resolverTransform.type = "osapi.jive.core.Group";
+    }
+    if(resolverTransform.type == 14){
+        resolverTransform.type = "osapi.jive.core.Space";
+    }
+    app.handleContext(resolverTransform);
+});

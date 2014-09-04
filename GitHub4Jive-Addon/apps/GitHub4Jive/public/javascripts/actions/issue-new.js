9ab88cf65152bee39d4d9e40b5971f460e0798fe
Glen Nicol <glen.nicol@live.com>
2014-09-04 15:50:59 -0700

    Added Validation to new issue title

--- GitHub4Jive-Addon/apps/GitHub4Jive/public/javascripts/actions/issue-new.js
+++ GitHub4Jive-Addon/apps/GitHub4Jive/public/javascripts/actions/issue-new.js
@@ -1,28 +1,34 @@
 $(function () {
+    var form = $("#newIssue");
+    form.validate();
+
     $("#btn_issue").click(function (e) {
-        var title = $("#Title").val();
-        var body = $("#Body").val();
 
-        var bodyPayload = {
-            "title": title,
-            "body": body
-        };
-        osapi.http.post({
-            'href': host + '/github/place/newIssue?' +
-                "ts=" + new Date().getTime()+
-                "&place=" + encodeURIComponent(placeUrl),
-            headers: { 'Content-Type': ['application/json'] },
-            'noCache': true,
-            'authz': 'signed',
-            'body': bodyPayload
-        }).execute(function (response) {
+        if(form.valid()) {
+            var title = $("#Title").val();
+            var body = $("#Body").val();
 
-            //alert( "status=" + response.status) ;
-            if ((response.status >= 400 && response.status <= 599) || !JSON.parse(response.content).success) {
-                alert("ERROR!" + JSON.stringify(response.content));
-            }
-            osapi.jive.core.container.closeApp();
-        });
+            var bodyPayload = {
+                "title": title,
+                "body": body
+            };
+            osapi.http.post({
+                'href': host + '/github/place/newIssue?' +
+                    "ts=" + new Date().getTime() +
+                    "&place=" + encodeURIComponent(placeUrl),
+                headers: { 'Content-Type': ['application/json'] },
+                'noCache': true,
+                'authz': 'signed',
+                'body': bodyPayload
+            }).execute(function (response) {
+
+                //alert( "status=" + response.status) ;
+                if ((response.status >= 400 && response.status <= 599) || !JSON.parse(response.content).success) {
+                    alert("ERROR!" + JSON.stringify(response.content));
+                }
+                osapi.jive.core.container.closeApp();
+            });
+        }
     });
 
     $("#btn_exit").click(function () {

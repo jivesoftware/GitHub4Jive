b6ead7e7d3120de1fd7f8acc8d3d757ab416379c
Glen Nicol <glen.nicol@live.com>
2014-09-11 13:36:05 -0700

    Tests, validation, and documentation of JiveContentBuilder

--- GitHub4Jive-Addon/test/library/JiveContentBuilder.js
+++ GitHub4Jive-Addon/test/library/JiveContentBuilder.js
@@ -21,13 +21,25 @@ var chaiAsPromised = require("chai-as-promised");
 
 chai.use(chaiAsPromised);
 
-describe("JiveContentBuilder", function () {
+describe.only("JiveContentBuilder", function () {
     var ContentBuilder = require("github4jive/JiveContentBuilder")
+
+    describe("constructor", function () {
+        it("should accept no arguments", function () {
+            var builder = new ContentBuilder();
+        });
+
+        it("should accept a valid Jive content object", function () {
+            var builder = new ContentBuilder();
+            var content = builder.discussion().subject("YO").body("a").build();
+            builder = new ContentBuilder(content);
+        });
+    });
 
     describe("#build", function () {
         it("should return the object being built", function () {
             var builder = new ContentBuilder();
-            var content = builder.document().subject("YO").body("").build();
+            var content = builder.discussion().subject("YO").body("a").build();
             content.should.have.property("type");
             content.should.have.property("content");
             content.content.should.have.property("type");
@@ -35,132 +47,180 @@ describe("JiveContentBuilder", function () {
 
         it("should throw when called without a type", function () {
             var builder = new ContentBuilder();
-            expect(function(){builder.build();}).to.throw(ContentBuilder.MISSING_TYPE);
+            expect(function(){builder.build();}).to.throw(builder.MISSING_TYPE);
         });
 
         it("should throw when called without calling body ", function () {
             var builder = new ContentBuilder();
             expect(function () {
-                builder.document().subject("YO").build();
-            }).to.throw(ContentBuilder.MISSING_BODY);
+                builder.discussion().subject("YO").build();
+            }).to.throw(builder.MISSING_BODY);
         });
 
-        it("should throw without calling subject on a document or discussion", function () {
+        it("should throw without calling subject on a discussion ", function () {
             var builder = new ContentBuilder();
             expect(function () {
-                builder.document().body("").build();
-            }).to.throw(ContentBuilder.MISSING_SUBJECT);
-            builder.reset();
-            expect(function () {
-                builder.discussion().body("").build();
-            }).to.throw(ContentBuilder.MISSING_SUBJECT);
+                builder.discussion().body("a").build();
+            }).to.throw(builder.MISSING_SUBJECT);
         });
 
-        it("should not throw without calling subject on a comment or message", function () {
+        it("should not throw without calling subject on a message", function () {
             var builder = new ContentBuilder();
             expect(function () {
-                builder.comment().body("").build();
-            }).to.not.throw(ContentBuilder.MISSING_SUBJECT);
-            builder.reset();
-            expect(function () {
-                builder.message().body("").build();
-            }).to.not.throw(ContentBuilder.MISSING_SUBJECT);
-        })
+                builder.message().body("a").build();
+            }).to.not.throw(builder.MISSING_SUBJECT);
+        });
 
         it("should accept callback to continue chaining", function () {
             var builder = new ContentBuilder();
             var firstContent = null;
-            builder.document().subject("YO").body("").build(function (finishedContent) {
+            builder.discussion().subject("YO").body("a").build(function (finishedContent) {
                 firstContent = finishedContent;
             }).body("chaining").build().should.not.deep.equal(firstContent);
-        })
+        });
     });
 
     describe("#reset", function () {
         it("should reset builder to new state", function () {
             var builder = new ContentBuilder();
             expect(function () {
-                builder.document().subject("YO").body("").reset().build();
-            }).to.throw(ContentBuilder.MISSING_TYPE);
+                builder.discussion().subject("YO").body("a").reset().build();
+            }).to.throw(builder.MISSING_TYPE);
         })
-    })
-
-    describe("#document", function () {
-        it("should create an object with type document", function () {
-            var builder = new ContentBuilder();
-            builder.document().subject("YO").body("").build().should.have.property("type").and.equal("document");
-        });
     });
 
     describe("#discussion", function () {
         it("should create an object with type discussion", function () {
             var builder = new ContentBuilder();
-            builder.discussion().subject("YO").body("").build().should.have.property("type").and.equal("discussion");
+            builder.discussion().subject("YO").body("a").build().should.have.property("type").and.equal("discussion");
         });
     });
 
     describe("#subject", function () {
         it("should create a subject member",function(){
             var builder = new ContentBuilder();
-            builder.discussion().body("").subject("YO").build()
+            builder.discussion().body("a").subject("YO").build()
                 .should.have.property("subject").and.equal("YO");
         });
-    })
+    });
+
+    describe("#body", function () {
+        it("should should set the content text field", function () {
+            var builder = new ContentBuilder();
+            var b = ["a","b"];
+            builder.discussion().subject("YO");
+            b.forEach(function (body) {
+                var content = builder.body(body).build();
+                content.content.text.should.equal(body);
+            })
+        });
+
+        it("should throw when body is empty", function () {
+            var builder = new ContentBuilder();
+
+            var emptyBodies = [function () {
+
+                builder.body("");
+            },function () {
+
+                builder.body(null);
+            },function () {
+
+                builder.body();
+            }];
+
+            emptyBodies.forEach(function (f) {
+                expect(f).to.throw(builder.EMPTY_BODY);
+            });
+        });
+
+        it("should throw if body is not a string", function () {
+            var builder = new ContentBuilder();
+            expect(function () {
+                builder.body({});
+            }).to.throw(builder.INVALID_BODY);
+        })
+    });
 
     describe("#bodyType", function () {
         it("should change the content type", function () {
             var builder = new ContentBuilder();
-            builder.discussion().body("").subject("YO").bodyType("text/plain").build()
-                .content.type.should.not.equal("text/html");
+            builder.discussion().body("a").subject("YO").bodyType("text/plain").build()
+                .content.type.should.equal("text/plain");
         });
+
+        it("should throw if type is empty", function () {
+            var builder = new ContentBuilder();
+
+            var invalid_types = [function () {
+                builder.bodyType("");
+            }, function () {
+                builder.bodyType(null);
+            }, function () {
+                builder.bodyType();
+            }];
+
+            invalid_types.forEach(function (f) {
+                expect(f).to.throw(builder.INVALID_BODY_TYPE);
+            })
+        })
     });
 
     describe("#parent", function () {
+        it("should only accept Jive uris", function () {
+            var builder = new ContentBuilder();
+            builder.parent("/contents/1");
+            builder.parent("/places/1");
+            builder.parent("/people/1");
 
+            var invalid_uris = ["a", "/a", "/a/1", "/places/ with whitespace" ];
+            invalid_uris.forEach(function (uri) {
+                expect(function () {
+                    builder.parent(uri);
+                }).to.throw(builder.INVALID_PARENT);
+            });
+        });
     });
 
     describe("#all", function () {
         it("should change visibility", function () {
             var builder = new ContentBuilder();
-            builder.document().subject("A").body("").all().build()
+            builder.discussion().subject("A").body("a").all().build()
                 .should.have.property("visibility").and.equal("all");
         });
     });
 
-    describe("#people", function () {
-        it("should change visibility", function () {
-            var builder = new ContentBuilder();
-            var content = builder.document().subject("A").body("").people(["glen.nicol"]).build();
-
-            content.should.have.property("visibility").and.equal("people");
-            content.should.have.property("users").and.contain("glen.nicol");
-        });
-
-        it("should throw if array is not passed", function () {
-            var builder = new ContentBuilder();
-            expect(function () {
-                builder.document().subject("A").body("").people();
-            }).to.throw(ContentBuilder.MISSING_USERS);
-        });
-    });
+//    describe("#people", function () {
+//        it("should change visibility", function () {
+//            var builder = new ContentBuilder();
+//            var content = builder.discussion().subject("A").body("a").people(["/people/1"]).build();
+//
+//            content.should.have.property("visibility").and.equal("people");
+//            content.should.have.property("users").and.contain("glen.nicol");
+//        });
+//
+//        it("should throw if array is not passed", function () {
+//            var builder = new ContentBuilder();
+//            expect(function () {
+//                builder.discussion().subject("A").body("a").people();
+//            }).to.throw(builder.MISSING_USERS);
+//        });
+//    });
 
     describe("#place", function () {
         it("should change visibility", function () {
             var builder = new ContentBuilder();
-            var content = builder.document().subject("A").body("").place("/content/123").build();
+            var content = builder.discussion().subject("A").body("a").place("/contents/123").build();
 
             content.should.have.property("visibility").and.equal("place");
-            content.should.have.property("parent").and.contain("/content/123");
+            content.should.have.property("parent").and.contain("/contents/123");
         });
 
         it("should throw if parent place is not passed", function () {
             var builder = new ContentBuilder();
             expect(function () {
-                builder.document().subject("A").body("").place();
-            }).to.throw(ContentBuilder.MISSING_PARENT);
+                builder.discussion().subject("A").body("a").place();
+            }).to.throw(builder.MISSING_PARENT);
         });
     })
-
-
-
-})
+});

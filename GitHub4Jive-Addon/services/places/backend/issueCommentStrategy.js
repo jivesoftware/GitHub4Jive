7d6f9b2f2e3446df033a5518718cec92a39d77d2
Glen Nicol <glen.nicol@live.com>
2014-09-12 15:46:34 -0700

    cleaned up issueCommentStrategy for place service

--- GitHub4Jive-Addon/services/places/backend/issueCommentStrategy.js
+++ GitHub4Jive-Addon/services/places/backend/issueCommentStrategy.js
@@ -25,6 +25,40 @@ module.exports = issueCommentStrategy;
 
 issueCommentStrategy.name = "Place_IssueComments";
 
+function commentDidNotOriginateFromJive(gitComment) {
+    return gitComment.indexOf("<!--Jive-->") != 0;
+}
+
+function formatGitComment(gitComment) {
+    gitComment = "<!--GitHub-->" + gitComment;
+}
+
+function addCommentToDiscussion(jiveApi, gitData, gitAuth, discussion){
+    if(discussion){
+        return gitHubFacade.getUserDetails(gitData.comment.user.login, gitAuth).then(function (user) {
+            var builder = new JiveContentBuilder();
+            var gitComment = gitData.comment.body;
+            formatGitComment(gitComment);
+            var comment = builder.message()
+                .body(gitComment)
+                .onBehalfOf(user.email || "", user.login)
+                .build();
+            return jiveApi.replyToDiscussion(discussion.contentID , comment).then(function (response) {
+                if (!response.success) {
+                    jive.logger.error("Error creating comment on " + discussion.subject, response);
+                }else{
+                    jiveApi.attachPropsToReply(response.entity.id,{fromGitHub: true}).then(function (response) {
+                        if (!response.success) {
+                            jive.logger.error("Error attaching props to comment", response);
+                        }
+                    })
+                }
+            })
+        });
+
+    }
+}
+
 /*
  * This strategy modifies anything in a place that is not on a tile in response to a created issue comment.
  * It could be split into separate strategies for fine grain configuration with the builder. Client code
@@ -43,42 +77,18 @@ issueCommentStrategy.setup = function(setupOptions) {
     var auth = gitHubFacade.createOauthObject( setupOptions.gitHubToken);
 
     return gitHubFacade.subscribeToRepoEvent(owner, repo, gitHubFacade.Events.IssueComment, auth, function (gitData) {
-
+        //GitHub comment event handler
         var gitComment = gitData.comment.body;
 
-        if(gitComment.indexOf("<!--Jive-->") != 0){
-            gitComment = "<!--GitHub-->" + gitComment;
-            helpers.getDiscussionForIssue(jiveApi,setupOptions.placeUrl, gitData.issue.id).then(function (discussion) {
-                if(discussion){
+        if(commentDidNotOriginateFromJive(gitComment)){
 
-                    return gitHubFacade.getUserDetails(gitData.comment.user.login, auth).then(function (user) {
-                        var builder = new JiveContentBuilder();
-                        var comment = builder.message()
-                            .body(gitComment)
-                            .onBehalfOf(user.email || "", user.login)
-                            .build();
-                        return jiveApi.replyToDiscussion(discussion.contentID , comment).then(function (response) {
-                            if (!response.success) {
-                                jive.logger.error("Error creating comment on " + discussion.subject);
-                                jive.logger.error(response);
-                            }else{
-                            jiveApi.attachPropsToReply(response.entity.id,{fromGitHub: true}).then(function (response) {
-                                if (!response.success) {
-                                    jive.logger.error("Error attaching props to comment");
-                                    jive.logger.error(response);
-                                }
-                            })
-                            }
-                        })
-                    });
-
-                }
-            }).catch(function (error) {
-                jive.logger.error(error);
+            helpers.getDiscussionForIssue(jiveApi,setupOptions.placeUrl, gitData.issue.id)
+                .then(function (discussion) {
+                    addCommentToDiscussion(jiveApi, gitData, auth, discussion);
+                })
+                .catch(function (error) {
+                    jive.logger.error(error);
             });
         }
-
-
     });
-
 };

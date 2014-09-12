74a311154ddd5e5afdb9eaf4e5919cc45175bd3c
Glen Nicol <glen.nicol@live.com>
2014-09-12 16:04:58 -0700

    missing q

--- GitHub4Jive-Addon/services/jive/backend/jiveCommentHandler.js
+++ GitHub4Jive-Addon/services/jive/backend/jiveCommentHandler.js
@@ -13,7 +13,7 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-
+var q = require("q");
 var gitFacade = require("github4jive/gitHubFacade");
 var helpers = require("./helpers");
 

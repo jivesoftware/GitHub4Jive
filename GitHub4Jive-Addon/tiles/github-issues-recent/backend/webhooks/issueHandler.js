89be5c7c42bfa40a8f95c627633be1780f5c75c5
rachoac <aronracho@yahoo.com>
2014-10-17 22:32:55 -0700

    Update issueHandler.js

--- GitHub4Jive-Addon/tiles/github-issues-recent/backend/webhooks/issueHandler.js
+++ GitHub4Jive-Addon/tiles/github-issues-recent/backend/webhooks/issueHandler.js
@@ -37,7 +37,6 @@ thisHandler.setup = function(setupOptions){
     // (1) create GitHub webhook, callback is /github/WebHookPortal; 
     return self.gitHubFacade.subscribeToRepoEvent(owner, repo, self.gitHubFacade.Events.Issues, auth, 
         // (2) contribute a webhook callback function to handle issue events
-        
         function () {
             // invoke the tile data push
             tileInstanceProcessor.processTileInstance(instance);
